7752d6a74b57aef39648e1c946f6ec4efffbf8ff
Сергей Мухин <onlydisco@yandex.ru>
2023-06-21 14:38:45 +0500

    initial commit

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/:resource": "/:resource",
+  })
+);
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("JSON Server is running");
+});
+
+module.exports = server;
